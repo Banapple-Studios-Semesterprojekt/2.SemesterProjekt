4dcc74a4bd7ae930eeab260f88522ccecdfe12b7
Emmsei <144018686+Emmsei@users.noreply.github.com>
2024-02-20 14:14:16 +0100

    Basic mechanics
    Player Controller + Interactor + Interactable + Test Scene

---
+++ TheButterflyEffect/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,106 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerInput playerInput;
+
+    [Header("References")]
+    [SerializeField] private Transform cam;
+    private CharacterController controller;
+
+    [Header("Player Properties")]
+    [SerializeField] private float walkSpeed = 3f;
+    [SerializeField] private float runSpeed = 6f;
+    [SerializeField] private float jumpPower = 3f;
+    [SerializeField] private float mouseSensitivity = 1f;
+    [SerializeField] private float gravity = -9.82f;
+
+    //Cached private variables
+    private Vector3 move;
+    private Vector3 fallVelocity;
+    private float xRotation;
+    private float currentSpeed;
+
+    private void Awake()
+    {
+        //Creating new input
+        playerInput = new PlayerInput();
+        playerInput.Enable();
+    }
+
+    private void Start()
+    {
+        //Referencing objects
+        controller = GetComponent<CharacterController>();
+
+        //Subscribing button events to functions
+        playerInput.Player.Jump.performed += Jumping;
+        playerInput.Player.Sprint.performed += Sprinting;
+        playerInput.Player.Sprint.canceled += Sprinting;
+
+        currentSpeed = walkSpeed;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Sprinting(InputAction.CallbackContext context)
+    {
+        //Tunary operator that is an if-statement in setting the currentSpeed
+        currentSpeed = context.performed ? runSpeed : walkSpeed;
+    }
+
+    private void Jumping(InputAction.CallbackContext context)
+    {
+        if(controller.isGrounded)
+        {
+            fallVelocity.y = jumpPower;
+        }
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+        UpdateCameraLook();
+    }
+
+    private void UpdateMovement()
+    {
+        //Getting player movement input
+        Vector2 moveInput = playerInput.Player.Movement.ReadValue<Vector2>();
+        move = transform.forward * moveInput.y + transform.right * moveInput.x;
+
+        //Resetting fallvelocity if grounded
+        if(controller.isGrounded && fallVelocity.y < 0)
+        {
+            fallVelocity.y = -2f;
+        }
+
+        //Calculating gravity
+        fallVelocity.y += gravity * Time.deltaTime;
+
+        //Actually moving
+        controller.Move((move * currentSpeed + fallVelocity) * Time.deltaTime);
+    }
+
+    private void UpdateCameraLook()
+    {
+        //Getting mouse input
+        Vector2 lookInput = playerInput.Player.CameraLook.ReadValue<Vector2>();
+        //Setting xRotation to be the y-mouse movement
+        xRotation -= lookInput.y * mouseSensitivity;
+        //Limiting xRotation to be only between -90 and 90
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //Setting cameras xRotation
+        cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        //Setting player bodys yRotation
+        transform.Rotate(Vector3.up * lookInput.x * mouseSensitivity);
+    }
+
+    public Transform GetCamera()
+    {
+        return cam;
+    }
+}
