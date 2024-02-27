312817237e2d1c5a2bc59141f7de3b6596428c99
CupcakeFlip <tiffanylea8@gmail.com>
2024-02-27 12:18:31 +0100

    Can now right click but left click is broken

---
+++ TheButterflyEffect/Assets/Scripts/InteractInventory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractInventory : MonoBehaviour {
+    public UnityEvent onInventory;
+
+    // Start is called before the first frame update
+    private void Start() {
+    }
+
+    // Update is called once per frame
+    private void Update() {
+    }
+}
--- TheButterflyEffect/Assets/Scripts/Interactable.cs
+++ TheButterflyEffect/Assets/Scripts/Interactable.cs
@@ -1,12 +1,27 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class Interactable : MonoBehaviour
 {
     public UnityEvent onInteract;
+    public UnityEvent onInventory;
 
-    public void DestroyObject()
+    private void Start()
     {
+        // Tilknyt input til Interact-metoden
+        PlayerController.playerInput.Player.PrimaryAction.performed += LeftClick;
+        PlayerController.playerInput.Player.RightClick.performed += RightClick;
+    }
+
+    public void LeftClick(InputAction.CallbackContext context)
+    {
+        Debug.Log("Left click");
         Destroy(gameObject);
+    }
+
+    public void RightClick(InputAction.CallbackContext context)
+    {
+        Debug.Log("Right click");
     }
 }
