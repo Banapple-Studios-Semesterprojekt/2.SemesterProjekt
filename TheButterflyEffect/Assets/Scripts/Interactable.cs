f556fd23735cbba6e02a071eee10dfc9d77c81c9
CupcakeFlip <tiffanylea8@gmail.com>
2024-03-05 08:45:00 +0100

    Revert "Can now right click but left click is broken"
    This reverts commit 312817237e2d1c5a2bc59141f7de3b6596428c99.

--- TheButterflyEffect/Assets/Scripts/Interactable.cs
+++ TheButterflyEffect/Assets/Scripts/Interactable.cs
@@ -1,27 +1,12 @@
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.InputSystem;
 
 public class Interactable : MonoBehaviour
 {
     public UnityEvent onInteract;
-    public UnityEvent onInventory;
 
-    private void Start()
+    public void DestroyObject()
     {
-        // Tilknyt input til Interact-metoden
-        PlayerController.playerInput.Player.PrimaryAction.performed += LeftClick;
-        PlayerController.playerInput.Player.RightClick.performed += RightClick;
-    }
-
-    public void LeftClick(InputAction.CallbackContext context)
-    {
-        Debug.Log("Left click");
         Destroy(gameObject);
-    }
-
-    public void RightClick(InputAction.CallbackContext context)
-    {
-        Debug.Log("Right click");
     }
 }
--- TheButterflyEffect/Assets/Scripts/Player/Interactor.cs
+++ TheButterflyEffect/Assets/Scripts/Player/Interactor.cs
@@ -5,37 +5,23 @@ public class Interactor : MonoBehaviour
 {
     private RaycastHit hit;
     private Transform cam;
-
     [Header("Raycast Settings")]
     [SerializeField] private float rayDistance = 2f;
-
     [SerializeField] private LayerMask rayMask;
 
     private Interactable currentInteractable;
-
+    
     private void Start()
     {
         cam = GetComponent<PlayerController>().GetCamera();
         PlayerController.playerInput.Player.PrimaryAction.performed += Interact;
-        PlayerController.playerInput.Player.RightClick.performed += Inventory;
     }
 
     private void Interact(InputAction.CallbackContext context)
     {
-        if (currentInteractable != null)
+        if(currentInteractable != null)
         {
             currentInteractable.onInteract?.Invoke();
-        }
-    }
-
-    private void Inventory(InputAction.CallbackContext context)
-    {
-        if (currentInteractable != null)
-        {
-            if (currentInteractable.gameObject.CompareTag("Inventory"))
-            {
-                currentInteractable.onInventory?.Invoke();
-            }
         }
     }
 
