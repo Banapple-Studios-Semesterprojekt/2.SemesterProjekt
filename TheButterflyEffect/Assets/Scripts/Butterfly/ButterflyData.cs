e34dfb709981fb4a9c2d5832f3f2843a018cba2d
CupcakeFlip <tiffanylea8@gmail.com>
2024-03-15 10:25:07 +0100

    Tilføjet danske og engelske navne for sommerfugle

--- TheButterflyEffect/Assets/Scripts/Butterfly/ButterflyData.cs
+++ TheButterflyEffect/Assets/Scripts/Butterfly/ButterflyData.cs
@@ -5,26 +5,44 @@ public class ButterflyData : ScriptableObject
 {
     public string butterflyName;
     public string description;
+
+    public ButterflyTypeEnglish typeEnglish; //Enum for the English butterfly type
+    public ButterflyTypeDanish typeDanish; //Enum for the Danish butterfly type
+
     public GameObject modelPrefab; // Reference to the 3D model prefab
 
     public float size;
 
-    public PairWith[] pairs; // Array to store the selection of races
-
-    public ButterflyType type;
-
-    // Add more variables as needed
+    public PairWithEnglish[] pairsEnglish; // Array to store the English names butterflies that can be paired with
+    public PairWithDanish[] pairsDanish; // Array to store the danish names butterflies that can be paired with
 }
 
 [System.Serializable]
-public class PairWith
+public class PairWithEnglish
 {
-    public ButterflyType type;
+    public ButterflyTypeEnglish typeEnglish;
 }
 
-public enum ButterflyType
+[System.Serializable]
+public class PairWithDanish
+{
+    public ButterflyTypeDanish typeDanish;
+}
+
+public enum ButterflyTypeEnglish
 {
     Monarch,
-    Swallowtail,
-    BlueMorpho
+    RedAdmiral,
+    CommonBrimstone,
+    HollyBlue,
+    TriangleBirdwing
+}
+
+public enum ButterflyTypeDanish
+{
+    Monark,
+    Admiral,
+    Citronsommerfugl,
+    Skovblåfugl,
+    TrogonopteraTrojana //Trekantfuglevingen, der findes ikke en dansk oversættelse
 }
--- TheButterflyEffect/Assets/Scripts/Butterfly/ButterflyDataEditor.cs
+++ TheButterflyEffect/Assets/Scripts/Butterfly/ButterflyDataEditor.cs
@@ -11,16 +11,5 @@ public class ButterflyDataEditor : Editor
 
         // Display default inspector GUI
         DrawDefaultInspector();
-
-        // Add a field for the model prefab
-        butterflyData.modelPrefab = (GameObject)EditorGUILayout.ObjectField("Model Prefab", butterflyData.modelPrefab, typeof(GameObject), false);
-
-        EditorGUILayout.LabelField("Custom GUI Elements", EditorStyles.boldLabel);
-        butterflyData.butterflyName = EditorGUILayout.TextField("Butterfly Name", butterflyData.butterflyName);
-        butterflyData.description = EditorGUILayout.TextField("Description", butterflyData.description);
-
-        butterflyData.size = EditorGUILayout.FloatField("Size", butterflyData.size);
-
-        // You can add more custom GUI elements here if needed
     }
 }
