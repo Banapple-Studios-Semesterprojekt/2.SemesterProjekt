e30cc8e6cab2228f156d6998afccc94871dac0bc
Emizu <113088577+XxEmilxX@users.noreply.github.com>
2024-04-12 14:09:40 +0200

    Added new breeding system script

--- TheButterflyEffect/Assets/BreedingSystem.cs
+++ TheButterflyEffect/Assets/BreedingSystem.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BreedingSystem : MonoBehaviour
 {
     [SerializeField] private InventorySlot slot1, slot2, result;
     [SerializeField] private ButteflyRecipe[] recipies;
+    [SerializeField] private Slider BreedSlider;
 
     private void Start()
     {
@@ -14,7 +17,6 @@ public class BreedingSystem : MonoBehaviour
     private void OnSlotChange()
     {
         print("Invetory Slot Changed!");
-        Invoke(nameof(TryBreed), 0.2f);
     }
 
     public void TryBreed()
@@ -22,14 +24,27 @@ public class BreedingSystem : MonoBehaviour
         foreach (ButteflyRecipe recipe in recipies)
         {
             ButterflyData output = TryGetOutput(recipe);
-            if(output != null)
+            if (output != null)
             {
-                InventoryItem outputItem = new InventoryItem(output);
-                result.SetInventorySlot(outputItem);
-                ClearInputSlots();
-                print("Sets Inventory Output Slot!");
+                StartCoroutine(Breed(recipe, output));
             }
         }
+    }
+
+    IEnumerator Breed(ButteflyRecipe butteflyRecipe, ButterflyData output)
+    {
+        float bredingtime = 0;
+        while (bredingtime < butteflyRecipe.breedTime)
+        {
+            yield return new WaitForSeconds(0.05f);
+            bredingtime += 0.05f;
+            BreedSlider.value = bredingtime / butteflyRecipe.breedTime;
+        }
+        InventoryItem outputItem = new InventoryItem(output);
+        result.SetInventorySlot(outputItem);
+        ClearInputSlots();
+        BreedSlider.value = 0;
+        print("Sets Inventory Output Slot!");
     }
 
     public void ClearInputSlots()
@@ -40,7 +55,7 @@ public class BreedingSystem : MonoBehaviour
 
     ButterflyData TryGetOutput(ButteflyRecipe recipe)
     {
-        if(slot1.currentItem != null && slot2.currentItem != null)
+        if (slot1.currentItem != null && slot2.currentItem != null)
         {
             if ((recipe.input1 == slot1.currentItem.item || recipe.input1 == slot2.currentItem.item) && (recipe.input2 == slot1.currentItem.item || recipe.input2 == slot2.currentItem.item))
             {
